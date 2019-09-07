4caed835bb72cda249275ad594076961ef812aa9
owaaquino <josejoshuaaquino@gmail.com>
2019-09-07 18:33:19 +0800

    initial commit

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Banner({title, subtitle, children}) {
+  return(
+  <div className="banner">
+    <h1>{title}</h1>
+    <div />
+    <p>{subtitle}</p>
+    {children}
+  </div>)
+}
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SingleRoom extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Single room page</h1>
+      </div>
+    );
+  }
+}
+
+export default SingleRoom;
